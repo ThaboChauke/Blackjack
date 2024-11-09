8c71a4d3b93fc585356c65d6c24e324f2bd0007a
ThaboChauke <chaukethabo16@gmail.com>
2024-11-09 20:07:29 +0200

    name section, minor fixes to getting dom elements

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,21 @@
 "use strict";
-document.getElementById("startGame").addEventListener("click", () => {
-    document.getElementById("landing-section").style.display = "none";
-});
+let player = {
+    name: "",
+    chips: 10000
+};
+const startGame = document.getElementById("startGame");
+const submitButton = document.getElementById("submit-name");
+const nameSection = document.getElementById("name-section");
+if (startGame) {
+    startGame.addEventListener("click", () => {
+        document.getElementById("landing-section").style.display = "none";
+        nameSection.style.display = "block";
+    });
+}
+if (submitButton) {
+    submitButton.addEventListener("click", () => {
+        player.name = document.getElementById("player-name").value;
+        nameSection.style.display = "none";
+        console.log("name " + player.name);
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-document.getElementById("startGame")!.addEventListener("click", () => {
-    document.getElementById("landing-section")!.style.display = "none"
-})
+let player : {name: string, chips: number} = {
+    name: "",
+    chips: 10000
+}
+
+const startGame = document.getElementById("startGame")
+const submitButton = document.getElementById("submit-name")
+const nameSection = document.getElementById("name-section")
+
+if (startGame) {
+    startGame.addEventListener("click", () => {
+        document.getElementById("landing-section")!.style.display = "none"
+        nameSection!.style.display = "block"
+    })
+}
+
+if (submitButton) {
+    submitButton.addEventListener("click", () => {
+        player.name = (document.getElementById("player-name") as HTMLInputElement).value
+        nameSection!.style.display = "none"
+    })
+}
