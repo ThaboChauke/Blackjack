48373efb4f4602dde949a4401742fc14aa62b43e
ThaboChauke <chaukethabo16@gmail.com>
2024-11-09 23:13:23 +0200

    get random card function

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,27 @@ if (submitButton) {
         gameSection!.style.display = "flex"
     })
 }
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    let cardValue = randomNumber > 10 ? 10 : randomNumber
+    let cardImage
+
+    let suits: string[] = ["hearts", "spades", "diamonds", "clubs"]
+    let randomSuit = suits[Math.floor(Math.random() * suits.length)]
+
+    if (randomNumber === 1) {
+        cardImage = "assets/images/ace_of_" + randomSuit + ".png"
+        cardValue = 11
+    } else if (randomNumber === 11) {
+        cardImage = "assets/images/jack_of_" + randomSuit + ".png"
+    } else if(randomNumber === 12) {
+        cardImage = "assets/images/queen_of_" + randomSuit + ".png"
+    } else if(randomNumber === 13) {
+        cardImage = "assets/images/king_of_" + randomSuit + ".png"
+    } else {
+        cardImage = "assets/images/" + randomSuit + "_of_" + randomSuit + ".png"
+    }
+
+    return { value: cardValue, cardImage: cardImage }
+}
