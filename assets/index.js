5cb518d5fc9eaa51f822092dc858b9840ae56c8c
ThaboChauke <chaukethabo16@gmail.com>
2024-11-09 22:30:53 +0200

    add game section

--- assets/index.js
+++ assets/index.js
@@ -6,6 +6,7 @@ let player = {
 const startGame = document.getElementById("startGame");
 const submitButton = document.getElementById("submit-name");
 const nameSection = document.getElementById("name-section");
+const gameSection = document.getElementById("game-section");
 if (startGame) {
     startGame.addEventListener("click", () => {
         document.getElementById("landing-section").style.display = "none";
@@ -16,6 +17,6 @@ if (submitButton) {
     submitButton.addEventListener("click", () => {
         player.name = document.getElementById("player-name").value;
         nameSection.style.display = "none";
-        console.log("name " + player.name);
+        gameSection.style.display = "flex";
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ let player : {name: string, chips: number} = {
 const startGame = document.getElementById("startGame")
 const submitButton = document.getElementById("submit-name")
 const nameSection = document.getElementById("name-section")
+const gameSection = document.getElementById("game-section")
 
 if (startGame) {
     startGame.addEventListener("click", () => {
@@ -18,5 +19,6 @@ if (submitButton) {
     submitButton.addEventListener("click", () => {
         player.name = (document.getElementById("player-name") as HTMLInputElement).value
         nameSection!.style.display = "none"
+        gameSection!.style.display = "flex"
     })
 }
